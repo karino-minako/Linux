7f4691ffd8f009dea4a4a4d027db961bece67807
karino-minako <irikiiomo11@gmail.com>
2020-10-04 20:40:53 +0900

    [Review]chapter10

--- [Review].rb
+++ [Review].rb
@@ -231,3 +231,48 @@ fi
 # fi
 
 5.sh -x シェルスクリプト名
+
+# 10章
+ip addr show dev enp0s3
+vi /etc/sysconfig/network-scripts/ifcfg-enp0s3
+systemctl restart network.service
+ip addr show dev enp0s3
+
+ip route add 192.168.2.0/24 via 192.168.1.1 dev enp0s3
+ip route
+ip route del 192.168.2.0/24 via 192.168.1.1
+ip route
+nslookup lpi.jp
+
+firewall-cmd --add-service=help
+firewall-cmd --add-service=https --permanent
+firewall-cmd --list-services
+firewall-cmd --list-services --permanent
+systemctl restart firewalld.service
+firewall-cmd --list-services
+systemctl stop firewalld.service
+systemctl mask firewalld.service
+yum -y install iptables-services
+systemctl start iptables.service
+systemctl enable iptables.service
+iptables -L
+vi /etc/sysconfig/iptables
+systemctl restart iptables.service
+iptables -L
+
+# 10章末テスト
+1.1
+
+2.動作：ターゲットや途中のゲートウェイへデータを送り、ゲートウェイから返答が戻ってくる時間を表示する。
+  # 送信先までに経由するゲートウェイを表示する
+  利用方法：traceroute ターゲット
+  # pingコマンドで送信先から正常なレスポンスがないとき、経由ゲートウェイのルーティングが正しいか調べる
+
+3.ip a
+
+4.# vi /etc/resolv.conf
+  # nameserver 8.8.8.8
+
+
+5.# vi /etc/hosts.deny
+  # sshd : 192.168.100.20
